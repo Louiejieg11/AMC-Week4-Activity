77c01e5ad45094687f35eb1183a5a9484c0e181f
Louiejieg11 <114630463+Louiejieg11@users.noreply.github.com>
2025-02-06 13:32:09 +0800

    ScrollView

---
+++ Activity 1/App.tsx
@@ -0,0 +1,252 @@
+import React from 'react';
+import { View, Text, ScrollView, StyleSheet,TouchableOpacity } from 'react-native';
+
+const App = () => {
+  return (
+    <ScrollView>
+        <View >
+        <Text>About 8,620,000 results
+Global web icon
+American Literature
+https://americanliterature.com
+Explore this image
+100 Great Short Stories
+100 Great Short Stories! There are thousands of short stories in our short story library, but with so many stories and so little time, we did you the favor of collecting our favorite 100 short stories in one place for you to enjoy. Happy …
+
+Other content from americanliterature.com
+Short Stories for Children - Short Stories and Classic Literature
+75 Short-Short Stories
+Short Stories
+See more
+Tags:Short StoriesShort Film
+ 
+Videos of Story
+bing.com › videos
+Story Writing | How to write a Story | Method | Examples | Exercise | Writing Skills
+10:45
+Story Writing | How to write a Story | Method | Examples | Exercise | Writing Skills
+1.2M viewsDec 30, 2020
+YouTubeNihir Shah
+English Kids Stories - Animated Stories For Kids || Moral Stories and Bedtime Stories For Kids
+45:26
+English Kids Stories - Animated Stories For Kids || Moral Stories and Bedtime Stories For Kids
+5.8M viewsJan 18, 2020
+YouTubeT-Series Kids Hut
+Best 5 Moral Stories For Kids | Learning Stories | Tia & Tofu Storytelling | Beddtime Stories
+27:11
+Best 5 Moral Stories For Kids | Learning Stories | Tia & Tofu Storytelling | Beddtime Stories
+1.2M viewsMay 26, 2023
+YouTubeT-Series Kids Hut
+Moral Stories for Kids | Tia & Tofu | English Stories for Kids | Bedtime Stories
+52:59
+Moral Stories for Kids | Tia & Tofu | English Stories for Kids | Bedtime Stories
+371.6K views6 months ago
+YouTubeT-Series Kids Hut
+Global web icon
+Storyberries
+https://www.storyberries.com
+Explore this image
+Storyberries - Fairy Tales, Bedtime Stories and Kids …
+Once upon a time in a faraway kingdom, a King wants a Princess... but Kathy doesn't want to be one! A story about true beauty... and the confidence to pursue what you love.
+
+AgeStoriesFairy TalesAudioBuy Our BooksBlog
+Global web icon
+American Literature
+https://americanliterature.com › short-stories-for-children
+Short Stories for Children - Short Stories and Classic Literature
+Enjoy hundreds of classic stories, fairy-tales, fables, and nursery rhymes for children. They help kids learn to read and make excellent bedtime stories.
+
+The Velveteen Rabbit
+The Velveteen Rabbit
+america…
+The Tale of Peter Rabbit
+The Tale of Peter Rabbit
+america…
+The Elves and the Shoema…
+The Elves and the Shoema…
+america…
+Rikki-Tikki-Tavi
+Rikki-Tikki-Tavi
+america…
+The Tortoise & the Hare
+The Tortoise & the Hare
+america…
+The Little Thief in the Pantry
+The Little Thief in the Pantry
+america…
+The Ugly Duckling
+The Ugly Duckling
+america…
+The Grassho…
+The Grassho…
+america…
+The Emperor's New Clot…
+The Emperor's New Clot…
+america…
+The Princess and the …
+The Princess and the …
+america…
+Tags:Short StoriesLiterature Stories
+Interesting reads - Story
+Powered by MSN
+
+Building Creative Great Modern Four Story Mud Villa House Design In The Forest
+
+I AM BUILDER
+The Best Short Story Collections That Keep You Reading
+Which of these captivating collections will you be picking up next?
+Good Housekeeping
+The Wingless Wonder: The Story of the Coleoptere
+Throughout the 1950s, aircraft designers around the world began developing a unique aircraft configuration, called a tail-sitter. Unlike conventional airplanes, tail sitting planes rested on their tails and used engine power alone to lift off the ground before transitioning to vertical flight, and returning to land vertically once again on their tail. The configuration, although technically challenging to develop, would allow aircraft to operate without runways, fundamentally changing how and where air forces could use their aircraft.
+Mustard
+The Best Story Arcs In My Hero Academia
+Relive some of the best story arcs in My Hero Academia.
+TheGamer
+A Ghost Story for Christmas: Woman of Stone – far too good to only exist as festive TV
+Heavens! Oh, it’s you, Doctor Blathery: forgive me, you gave me an awful fright. You see it’s the queerest thing: this little stone statue I inherited with the cottage when I moved to this sleepy village from London (where everybody hates me because I’m from London), well, it seems to me … Oh, you shall call me half-mad! It seems to be moving around from room to room when I’m not looking. I swear it to you: last night, while I was reading by the...
+The Guardian
+Global web icon
+American Literature
+https://americanliterature.com › short-shor…
+Explore this image
+75 Short-Short Stories
+A poke at idioms, sci-fi style. If we read everything literally, we'd go mad. You'll be "in stitches" by the time you're done reading Dick's "side-splitting" story!
+
+Tags:Short FilmA List of Short Stories10 Greates Shorth StoriesGood Short Stories
+Global web icon
+storyplanets.com
+https://storyplanets.com
+Explore this image
+100 Moral Stories for Kids » Story Planets
+Jan 30, 2025 · Story Planets is a world of fascinating stories with the objective of sharing knowledge and learning. We have large collection of kids stories covering moral and 1 minute stories, fun facts, tech and lifestyle articles!!!
+
+Tags:MoralChild
+Explore story
+
+Story Book Cartoon
+
+Grade 2 Story
+
+A Never Ending Story
+
+Story Writing Steps
+
+Grade 1 Story
+
+Story for Grade 5
+
+Story Outline Template
+
+Story Time Clip Art
+
+Story Time Animated
+
+Story Graphic Organizer
+
+5 Elements of the Story
+
+Story Books for Children
+
+Story Book Art
+
+Long a Short Story
+
+Story Map Graphic Organizer
+
+Story Planning Sheet
+
+Story Book Making
+
+Story Clip Art
+
+Story Structure Diagram
+
+Story Writing Chart
+
+Story Writing Cartoon
+
+Story Writing Tips for Kids
+
+Story for Kids
+
+Story Time Cartoon
+
+Story Concept Map
+
+Story Plot Mountain
+
+Story so Far
+
+Greedy Dog Story
+
+Story Poems
+
+Moral Story
+Global web icon
+Interesting Literature
+https://interestingliterature.com › best-v…
+Explore this image
+10 of the Best Very Short Stories That Can Be Read …
+Enjoy some of the best very short stories by famous authors, from Chekhov to Borges, in just a few pages. Find links to read the stories online and analyses of their themes and styles.
+
+Tags:Literature StoriesA Very Short Story in EnglishBest Very Short Stories
+Global web icon
+Library of Short Stories
+https://www.libraryofshortstories.com
+Explore this image
+Library of Short Stories - Read Classic Short Stories …
+“God alone understands the state of a man’s soul when he takes his own life; but men know nothing about it.” - A Story Without an End
+
+Tags:Short StoriesRead
+Global web icon
+Kids Story
+https://kidsstory.org
+Explore this image
+Short Kids Stories - Fun Tales for Children
+Short Kids Story Donkeys | Engaging Tales for Children […] ‘The Two Donkeys,’ a short kids story...
+
+Tags:Short StoriesStory
+Global web icon
+Storyline Online
+https://storylineonline.net
+Explore this image
+Storyline Online - Home
+A two-time Emmy-nominated program of the SAG-AFTRA Foundation, Storyline Online features actors reading children's books to inspire a love of reading worldwide.
+
+Global web icon
+storyplanets.com
+https://storyplanets.com
+100 Popular Children Stories of All Time » Story Planets
+Jan 18, 2025 · Story Planets is a world of fascinating stories with the objective of sharing knowledge and learning. We have large collection of kids stories covering moral and 1 minute …
+
+Tags:Children's literaturePlanet
+Related searches for story
+examples of a story
+free stories online reading
+stories online free
+funny story</Text>
+        </View>
+    </ScrollView>
+  );
+  
+};
+const styles = StyleSheet.create({
+  container: {
+      borderWidth: 1,
+      padding: 20,
+      backgroundColor: "#9DC08B",
+  
+  },
+  textBold:{
+
+      fontSize: 20,
+      textAlign: "center",
+      fontWeight: 'bold',
+  },
+  textItalic:{
+      fontStyle: "italic"
+  }
+  
+  })
+
+export default App;
